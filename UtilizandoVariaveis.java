c78f46a1beba13d3d5ff87b61816dc1feb4edd48
Victória Amaral <ttoiav.aamaral@gmail.com>
2024-05-10 15:19:39 -0300

    SenaiFJ202401, Utilizando Variaveis

---
+++ UtilizandoVariaveis.java
@@ -0,0 +1,16 @@
+public class UtilizandoVariaveis {
+    public static void main(String[] args) {
+		//Declarando as variaveis
+		int valor_Inicial=9;
+		float valorFracionado=1.8f;
+		double valor_Decimal= 35.87;
+		boolean pergunta = true;
+		String palavra = "Tubarão, Sh4rk";
+		
+		System.out.println("Valor Inicial: "+valor_Inicial);
+        System.out.println("Valor Fracionado: "+valorFracionado);
+        System.out.println("Valor Decimal: "+valor_Decimal);
+        System.out.println("Valor: "+pergunta);
+        System.out.println("Valor: "+palavra);
+	}
+}
