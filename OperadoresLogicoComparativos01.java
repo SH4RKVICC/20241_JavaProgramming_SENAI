09a823c2fedcef3925ac73e477d68a76cdfb527c
Victória Amaral <ttoiav.aamaral@gmail.com>
2024-05-10 16:07:25 -0300

    SenaiFJ202401, True e False

---
+++ OperadoresLogicoComparativos01.java
@@ -0,0 +1,19 @@
+public class OperadoresLogicoComparativos01 {
+
+	public static void main(String[] args) {
+        //declarando variaveis
+		int valor_01=9;
+		int valor_02=6;
+		int valor_03=2;
+		int valor_04=5;
+		boolean total;
+		
+        //realizando operação
+		total = valor_01 <= valor_04;
+
+		//emitindo resultado
+		System.out.println("Resposta: "+total);
+
+	}
+
+}
