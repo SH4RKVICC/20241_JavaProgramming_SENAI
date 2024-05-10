438dc513ed7f222feff45e62913709dbef48e6af
Victória Amaral <ttoiav.aamaral@gmail.com>
2024-05-10 15:23:01 -0300

    SenaiFJ202401, Utilizando Adição

---
+++ OperadoresAritmeticos01.java
@@ -0,0 +1,20 @@
+package Aula_02;
+
+public class OperadoresAritmeticos01 {
+	
+	public static void main(String[] args) {
+		//declarando as variaveis
+		int valor_01;
+		int valor_02;
+		int total;
+		//atribuindo os valores as variaveis
+		valor_01=23;
+		valor_02=45;
+		//realizando calculo
+		total=valor_01+valor_02;
+		//exibindo a mensagem para o usúario
+		System.out.println("Total: "+total);
+		
+	}
+	
+}
