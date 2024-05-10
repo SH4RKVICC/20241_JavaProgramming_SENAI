f1923f7a38a2938d367edfe056fceb337411ae91
Victória Amaral <ttoiav.aamaral@gmail.com>
2024-05-10 16:21:45 -0300

    SenaiFJ202401, Utilizando ||

---
+++ OperadoresLogicoComparativos02.java
@@ -0,0 +1,12 @@
+public class OperadoresLogicoComparativos02 {
+    int valor_01=9;
+		int valor_02=6;
+		int valor_03=2;
+		int valor_04=5;
+		boolean total;
+
+        total = valor_01 <= valor_02 || valor_01 == valor_03 || valor_02==00;
+
+        System.out.println("Resposta: "+total);
+    
+}
