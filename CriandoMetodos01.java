b67fffa23e6549f6586f2cc9e93ee36ed28d2bf3
Victória Amaral <ttoiav.aamaral@gmail.com>
2024-05-15 14:36:32 -0300

    SenaiFJ202401, Implementando Subtração

--- CriandoMetodos01.java
+++ CriandoMetodos01.java
@@ -1,11 +1,12 @@
 import java.util.Scanner;
+
 public class CriandoMetodos01 {
 	
 	public static void main(String[] args) {
-		somar();
+		calculo();
 	}
-	public static void somar() {
-		int valor01, valor02, total;
+	public static void calculo() {
+		int valor01, valor02, soma, sub;
 		Scanner ler = new Scanner(System.in);
 		
 		System.out.println("Digite um valor: ");
@@ -14,8 +15,10 @@ public class CriandoMetodos01 {
 		System.out.println("Digite outro valor: ");
 		valor02 = ler.nextInt();
 		
-		total = valor01 + valor02;
+		soma = valor01 + valor02;
+		sub = valor01 - valor02;
 		
-		System.out.println("Total: "+total);
+		System.out.println("Resultado da Soma: "+soma);
+		System.out.println("Resultado da Subtração: "+sub);
 	}
 }
