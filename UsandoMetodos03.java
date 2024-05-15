7fce96b6de68035e5d7422a444350e9c79cc90bd
Victória Amaral <ttoiav.aamaral@gmail.com>
2024-05-15 17:24:04 -0300

    SenaiFJ202401, Aprendendo a utilizar Return02

---
+++ UsandoMetodos03.java
@@ -0,0 +1,13 @@
+public class UsandoMetodos03 {
+
+	public static void main(String[] args) {
+		String receber = cumprimentar();
+		System.out.println(receber);
+
+	}
+	public static String cumprimentar() {
+		String falar = "Boa tarde";
+		return falar;
+	}
+
+}
