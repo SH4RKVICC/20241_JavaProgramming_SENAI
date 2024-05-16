054da00bec388275808e67181c321c2f722eefb6
Victória Amaral <ttoiav.aamaral@gmail.com>
2024-05-16 14:00:07 -0300

    SenaiFJ202401, Adicionando Código a PedindoDados.java

---
+++ PedindoDados.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class PedindoDados {
+	
+	public double dado01() {
+		Scanner ler = new Scanner(System.in);
+		System.out.println("Digite um valor: ");
+		double valor01 = ler.nextDouble();
+		return valor01;
+		
+	}
+	public double dado02() {
+		Scanner ler = new Scanner(System.in);
+		System.out.println("Digite outro valor: ");
+		double valor02 = ler.nextDouble();
+		return valor02;
+		
+		calculadora.calculo(valor01, valor02);
+	}
+}
