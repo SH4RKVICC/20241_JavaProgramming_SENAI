81b3e74bb0d86126cf6e1e8aac03173a1966e7f0
Victória Amaral <ttoiav.aamaral@gmail.com>
2024-05-22 16:47:19 -0300

    SenaiFJ202401, Adicionando README a Aula_01

---
+++ Aula_06/SoletrandoSenai.java
@@ -0,0 +1,14 @@
+package Aula06;
+
+public class SoletrandoSenai {
+
+	public static void main(String[] args) {
+		String nome = "Senai";
+		
+		for (int i = 0; i <nome.length(); i++) {
+			System.out.println("Volta: "+nome.charAt(i));
+		}
+
+	}
+
+}
