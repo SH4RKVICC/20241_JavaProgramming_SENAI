8441213d4a52bf4882e8c2f7c1377aca7e3e8b89
Victória Amaral <ttoiav.aamaral@gmail.com>
2024-05-22 17:00:30 -0300

    SenaiFJ202401, Aprendendo For!

--- Aula_06/UsandoLoopFor01.java
+++ Aula_06/UsandoLoopFor01.java
@@ -6,7 +6,7 @@ public class UsandoLoopFor01 {
 
 	public static void main(String[] args) {
 		for (int i = 0; i < 10; i++) {
-			System.out.println("Volta: "+i);
+			System.out.println("volta: "+i);
 			
 		}
 
---
+++ Aula_06/UsandoLoopFor02.java
@@ -0,0 +1,22 @@
+package Aula06;
+
+import java.util.Scanner;
+
+public class UsandoLoopFor02 {
+
+	public static void main(String[] args) {
+		int[]numeros = new int[10];
+		
+		Scanner ler = new Scanner (System.in);
+		
+		for (int i = 0; i < numeros.length; i++) {
+			System.out.println("Digite um número: ");
+			numeros[i]=ler.nextInt();
+		}
+		for (int i = 0; i < numeros.length; i++) {
+			System.out.println("Número: "+numeros[i]+"Posição: "+i);
+		}
+
+	}
+
+}
