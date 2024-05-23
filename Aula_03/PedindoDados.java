c086f37588b01997f5edbd70f6d3c21fc004ce10
Victória Amaral <ttoiav.aamaral@gmail.com>
2024-05-23 15:17:26 -0300

    SenaiFJ202401, Organizando a estrutura de PedindoDados.

--- Aula_03/PedindoDados.java
+++ Aula_03/PedindoDados.java
@@ -3,20 +3,29 @@ package Aula_03;
 import java.util.Scanner;
 
 public class PedindoDados {
-	
-	public double dado01() {
-		Scanner ler = new Scanner(System.in);
-		System.out.println("Digite um valor: ");
-		double valor01 = ler.nextDouble();
-		return valor01;
-		
-	}
-	public double dado02() {
-		Scanner ler = new Scanner(System.in);
-		System.out.println("Digite outro valor: ");
-		double valor02 = ler.nextDouble();
-		return valor02;
-		
-		calculadora.calculo(valor01, valor02);
-	}
+    
+    public double dado01() {
+        double valor01;
+        try (Scanner ler = new Scanner(System.in)) {
+            System.out.println("Digite um valor: ");
+            valor01 = ler.nextDouble();
+        }
+        return valor01;
+    }
+    
+    public double dado02() {
+        double valor02;
+        try (Scanner ler = new Scanner(System.in)) {
+            System.out.println("Digite outro valor: ");
+            valor02 = ler.nextDouble();
+        }
+        return valor02;
+    }
+
+    public void realizarCalculo() {
+        double valor01 = dado01();
+        double valor02 = dado02();
+        Calculo calculadora = new Calculo();
+        calculadora.calculo(valor01, valor02);
+    }
 }
