c68d6ffffa9c88bda2e7bf412569d77f2ed4e9d7
Victória Amaral <ttoiav.aamaral@gmail.com>
2024-05-24 13:11:06 -0300

    SenaiFJ202401, Adaptando Estudio.java..

--- ProjetoPessoa/Estudio.java
+++ ProjetoPessoa/Estudio.java
@@ -4,17 +4,17 @@ public class Estudio {
     public static void main(String[] args) {
         PessoaJuridica estudio = new PessoaJuridica();
 
-        estudio.NomeFantasia = "Vitrine Filmes";
-        estudio.endereco = "Rua Armando Pinto, 142";
-        estudio.CNPJ = "11.620.976/0001-83";
-        estudio.tel = "(11)3081-0968";
-        estudio.email = "silvia.cruz@vitrinefilmes.com.br";
+        estudio.setNF("Vitrine Filmes");
+        estudio.setEnd("Rua Armando Pinto, 142");
+        estudio.setCNPJ("11.620.976/0001-83");
+        estudio.setTel("(11)3081-0968");
+        estudio.setEmail("silvia.cruz@vitrinefilmes.com.br");
 
-        System.out.println("Nome Fantasia: "+estudio.NomeFantasia+
-                            "\nEndereço: "+estudio.endereco+
-                            "\nCNPJ: "+estudio.CNPJ+
-                            "\nTelefone: "+estudio.tel+
-                            "\nEmail: "+estudio.email);
+        System.out.println("Nome Fantasia: "+estudio.getNF()+
+                            "\nEndereço: "+estudio.getEnd()+
+                            "\nCNPJ: "+estudio.getCNPJ()+
+                            "\nTelefone: "+estudio.getTel()+
+                            "\nEmail: "+estudio.getEmail());
     }
     
 }
