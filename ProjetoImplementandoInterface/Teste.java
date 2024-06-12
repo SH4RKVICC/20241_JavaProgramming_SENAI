858f16a16b4a95254ca03d99371a5bb6297fc75d
Victória Amaral <ttoiav.aamaral@gmail.com>
2024-06-12 17:05:46 -0300

    SenaiFJ202401, Implementando Teste nos calcúlos do Projeto Interface.

--- ProjetoImplementandoInterface/Teste.java
+++ ProjetoImplementandoInterface/Teste.java
@@ -4,14 +4,14 @@ public class Teste {
     
     public static void main(String[] args) {
         formaGeometrica t = new Triangulo(5, 8, 3, 4, 5);
-        formaGeometrica c = new Circulo();
-        formaGeometrica r = new Retangulo();
+        formaGeometrica c = new Circulo(4.3);
+        formaGeometrica r = new Retangulo(4, 12);
 
         System.out.println("Área do Triangulo: "+ t.calcularArea());
         System.out.println("Perimetro do Triângulo: "+ t.calcularPerimetro());
         System.out.println("Área do Circulo: "+ c.calcularArea());
         System.out.println("Perimetro do Circulo: "+ c.calcularPerimetro());
-        System.out.println("Área do Retângulo: "+ r.calcularArea(););
+        System.out.println("Área do Retângulo: "+ r.calcularArea());
         System.out.println("Perimetro do Retângulo: "+ r.calcularPerimetro());
     }
 }
