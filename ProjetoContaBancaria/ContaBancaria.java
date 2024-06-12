f9f96609b6e876b99cba67cfd943a44e7bd265dd
Victória Amaral <ttoiav.aamaral@gmail.com>
2024-06-12 15:12:36 -0300

    SenaiFJ202401, Implementando Mudanças no Projeto Conta Bancaria.

--- ProjetoContaBancaria/ContaBancaria.java
+++ ProjetoContaBancaria/ContaBancaria.java
@@ -1,47 +1,30 @@
 package ProjetoContaBancaria;
 
 public class ContaBancaria {
-    private String nome, cod, metodo, op;
-     private double saldo;
+    private String nomeTitular, cod, metodo, op;
+    private double saldo;
 
-    public String getNome(){
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCod(){
-        return cod;
-    }
-
-    public void setNumConta(String nome) {
+    public ContaBancaria(String nomeTitular, String cod, double saldo) {
+        this.nomeTitular = nomeTitular;
         this.cod = cod;
+        this.saldo = saldo;
     }
 
     public void definirMetodo(String op) {
         switch (op) {
             case "1":
                 this.metodo = "Sacar.";
-                System.out.println("Metodo saque definido!");
+                System.out.println("Método saque definido!");
                 break;
             case "2":
                 this.metodo = "Depositar.";
-                System.out.println("Metodo deposito definido!");
+                System.out.println("Método deposito definido!");
                 break;
             case "3":
                 this.metodo = "Consultar saldo.";
-                System.out.println("Metodo consulta definido!");
+                System.out.println("Método consulta definido!");
                 break;
         }
     }
-
-    public Double getSaldo(){
-        return saldo;
-    }
-
-    public void setSaldo(Double saldo) {
-        this.saldo = saldo;
-    }
+    
 }
---
+++ ProjetoContaBancaria/Main.java
@@ -0,0 +1,15 @@
+package ProjetoContaBancaria;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ContaBancaria m = new ContaBancaria("Milena Campos", "0000", 3270);
+
+        ContaBancaria v = new ContaBancaria("Victória Amaral", "0001", 1870);
+
+        ContaBancaria l = new ContaBancaria("Lorena Dantas", "0002", 2800);
+
+        ContaBancaria n = new ContaBancaria("Natalia", "0003", 1500)
+    }
+    
+}
