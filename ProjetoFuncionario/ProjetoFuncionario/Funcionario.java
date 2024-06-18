c08039c5f5965d3e08d525d3e9945aee5020910e
Victória Amaral <ttoiav.aamaral@gmail.com>
2024-06-18 16:17:47 -0300

    classes(projeto-funcionario): atualizando projeto.

--- ProjetoFuncionario/ProjetoFuncionario/Funcionario.java
+++ ProjetoFuncionario/ProjetoFuncionario/Funcionario.java
@@ -2,7 +2,9 @@ package ProjetoFuncionario;
 
 public abstract class Funcionario {
 
+    @SuppressWarnings("unused")
     private String nome, tipo;
+    @SuppressWarnings("unused")
     private double sal;
 
     public Funcionario(String nome, String tipo, Double sal) {
