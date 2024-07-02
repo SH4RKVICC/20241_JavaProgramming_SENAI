24f427183761a58e4cdf2c730a94ec18c977cebb
Victória Amaral <ttoiav.aamaral@gmail.com>
2024-07-02 15:11:23 -0300

    matrizes(matriz-diagonal): verificando se a matriz é diagonal.

---
+++ Matrizes/MatrizDiagonal.java
@@ -0,0 +1,28 @@
+package Matrizes;
+
+public class MatrizDiagonal {
+    public static void main(String[] args) {
+        int[][] matriz = {
+            {1, 0, 0},
+            {6, 2, 0},
+            {0, 0, 3}
+        };
+
+        boolean isDiagonal = verificarMatrizDiagonal(matriz);
+        System.out.println("A matriz é diagonal? "+isDiagonal);
+    }
+
+    public static boolean verificarMatrizDiagonal(int[][] matriz) {
+        int n = matriz.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i != j && matriz[i][j] != 0) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+    
+}
